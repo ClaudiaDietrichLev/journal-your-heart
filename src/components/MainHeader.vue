dc3c66b10d60f6309d5835dacf12703a75bd1865
claudia.sollinger@gmail.com <claudia.sollinger@gmail.com>
2022-11-27 20:33:39 +0100

    Feat: Header Layout

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>Journaling is writing your heart on paper</h1>
+  </header>
+</template>
+<script>
+export default {
+  name: "MainHeader",
+};
+</script>
+<style scoped>
+header {
+  background-color: var(--color-accent);
+  padding: 2em;
+}
+
+h1 {
+  margin: 0;
+  text-align: center;
+  color: var(--color-white);
+}
+</style>
